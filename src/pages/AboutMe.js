1eecf1d1054ed4188e72194e8839ad3dc9b7a4b8
PrasadSawant11121999 <prasad11121999@gmail.com>
2024-06-09 20:00:24 +0530

    About me changed

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,24 +6,21 @@ import profileImage from '../asset/myprofile.png';
 
 const AboutMe = () => {
   return (
-    <div>
+    <div style={{background: "linear-gradient(-45deg, #4e96d8, #46d060)", color:"white"}}>
       <NavigationBar />
       <Container className="py-5">
         <Row>
           <Col md={6}>
-            <h2>About Me</h2>
+            <h2 style={{paddingTop:"100px"}}>About Me</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis ligula sit amet odio commodo hendrerit.
-              Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Hello! I'm Prasad Sawant, a Master of Mathematics candidate with a strong background in software development and a keen interest in data science. My academic journey has equipped me with deep knowledge in statistical analysis, mathematical modeling, and algorithm development. With several years of experience in software development, I am proficient in Python, R, and C#, and have worked extensively with frameworks like ASP.Net MVCs, StreamLit and TensorFlow. 
             </p>
             <p>
-              Morbi sagittis tortor et eros elementum, ut venenatis ex lacinia. Nullam non enim nec arcu volutpat
-              porttitor eget a eros. Vivamus eget accumsan lectus, ac convallis sapien. Ut viverra elit at luctus
-              vestibulum.
+            I have a passion for transforming data into actionable insights and have developed projects such as stock market prediction models, sales data visualization dashboards, and algorithmic trading strategies. I am always eager to tackle complex problems and contribute to innovative data-driven solutions. Feel free to explore my portfolio to see my projects and connect with me on LinkedIn/GitHub for potential collaborations or opportunities.
             </p>
           </Col>
           <Col md={6} className="text-center">
-            <Image src={profileImage} roundedCircle fluid />
+            <Image src={profileImage} roundedCircle fluid style={{backgroundColor:"white"}}/>
           </Col>
         </Row>
       </Container>
